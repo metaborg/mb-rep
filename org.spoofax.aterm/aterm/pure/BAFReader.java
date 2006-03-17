a8a1c1754b6a2dca34dd91083068eaca0ea0f622
Karl Trygve Kalleberg <karltk@gmail.com>
2006-03-17 10:27:49 +0000

    Initial import of ATerm library with added BAF support.
    svn path=/spoofax/trunk/spoofax/org.spoofax.aterm/; revision=14701

---
+++ org.spoofax.aterm/aterm/pure/BAFReader.java
@@ -0,0 +1,314 @@
+/*
+ * Created on 13.aug.2005
+ *
+ * Copyright (c) 2005, Karl Trygve Kalleberg <karltk@ii.uib.no>
+ * 
+ * Licensed under the GNU General Public License, v2
+ */
+package aterm.pure;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import aterm.AFun;
+import aterm.ATerm;
+import aterm.ATermList;
+import aterm.ParseError;
+import aterm.pure.PureFactory;
+
+public class BAFReader {
+
+    private static final int BAF_MAGIC = 0xBAF;
+
+    private static final int BAF_VERSION = 0x300;
+
+    private static final int HEADER_BITS = 32;
+
+    private BitStream reader;
+
+    private int nrUniqueSymbols = -1;
+
+    private SymEntry[] symbols;
+
+    private PureFactory factory;
+
+    class SymEntry {
+
+        public AFun fun;
+
+        public int arity;
+
+        public int nrTerms;
+
+        public int termWidth;
+
+        public ATerm[] terms;
+
+        public int[] nrTopSyms;
+
+        public int[] symWidth;
+
+        public int[][] topSyms;
+
+    }
+
+    public BAFReader(PureFactory factory, InputStream inputStream) {
+        this.factory = factory;
+        reader = new BitStream(inputStream);
+    }
+
+    public ATerm readFromBinaryFile(boolean headerAlreadyRead) throws ParseError, IOException {
+
+        if(!headerAlreadyRead && !isBinaryATerm(reader))
+            throw new ParseError("Input is not a BAF file");
+        
+        int val = reader.readInt();
+
+        if (val != BAF_VERSION)
+            throw new ParseError("Wrong BAF version, giving up");
+
+        nrUniqueSymbols = reader.readInt();
+        int nrUniqueTerms = reader.readInt();
+
+        debug("" + nrUniqueSymbols + " unique symbols");
+        debug("" + nrUniqueTerms + " unique terms");
+
+        symbols = new SymEntry[nrUniqueSymbols];
+
+        readAllSymbols();
+
+        int i = reader.readInt();
+
+        return readTerm(symbols[i]);
+    }
+
+    public static boolean isBinaryATerm(InputStream in) throws IOException {
+        return isBinaryATerm(new BitStream(in));
+    }
+    
+    private static boolean isBinaryATerm(BitStream in) throws IOException {
+        int w0 = in.readInt();
+        int w1 = in.readInt();
+        
+        if (w0 == 0 && w1 == BAF_MAGIC)
+            return true;
+        
+        return false;
+    }
+
+    
+    private void debug(String s) {
+        // System.out.println(s);
+    }
+
+    private ATerm readTerm(SymEntry e) throws ParseError, IOException {
+        int arity = e.arity;
+        ATerm[] args = new ATerm[arity];
+
+        debug("readTerm() - " + e.fun.getName() + "[" + arity + "]");
+
+        for (int i = 0; i < arity; i++) {
+            int val = reader.readBits(e.symWidth[i]);
+            debug(" [" + i + "] - " + val);
+            debug(" [" + i + "] - " + e.topSyms[i].length);
+            SymEntry argSym = symbols[e.topSyms[i][val]];
+
+            val = reader.readBits(argSym.termWidth);
+            if (argSym.terms[val] == null) {
+                debug(" [" + i + "] - recurse");
+                argSym.terms[val] = readTerm(argSym);
+            }
+
+            if (argSym.terms[val] == null)
+                throw new ParseError("Cannot be null");
+
+            args[i] = argSym.terms[val];
+        }
+
+        /*
+        switch (e.fun.getType()) {
+        case ATerm.BLOB:
+            reader.flushBitsFromReader();
+            String t = reader.readString();
+            return factory.makeBlob(t.getBytes());
+        case ATerm.PLACEHOLDER:
+            return factory.makePlaceholder(args[0]);
+        }
+        */
+        
+        if (e.fun.getName().equals("<int>")) {
+            int val = reader.readBits(HEADER_BITS);
+            return factory.makeInt(val);
+        }
+        if (e.fun.getName().equals("<real>")) {
+            reader.flushBitsFromReader();
+            String s = reader.readString();
+            return factory.makeReal(new Double(s).doubleValue());
+        }
+        if (e.fun.getName().equals("[_,_]")) {
+            debug("--");
+            for (int i = 0; i < args.length; i++)
+                debug(" + " + args[i].getClass());
+            return ((ATermList) args[1]).insert(args[0]);
+        }
+        if (e.fun.getName().equals("[]"))
+            return factory.makeList();
+
+        // FIXME: Add annotation case
+        // FIXME: Add blob case
+        // FIXME: Add placeholder case
+        
+        debug(e.fun + " / " + args);
+        for (int i = 0; i < args.length; i++)
+            debug("" + args[i]);
+        return factory.makeAppl(e.fun, args);
+    }
+
+    private void readAllSymbols() throws IOException {
+
+        for (int i = 0; i < nrUniqueSymbols; i++) {
+            SymEntry e = new SymEntry();
+            symbols[i] = e;
+
+            AFun fun = readSymbol();
+            e.fun = fun;
+            int arity = e.arity = fun.getArity();
+
+            int v = reader.readInt();
+            e.nrTerms = v;
+            e.termWidth = bitWidth(v);
+            // FIXME: original code is inconsistent at this point!
+            e.terms = (v == 0) ? null : new ATerm[v];
+
+            if (arity == 0) {
+                e.nrTopSyms = null;
+                e.symWidth = null;
+                e.topSyms = null;
+            } else {
+
+                e.nrTopSyms = new int[arity];
+                e.symWidth = new int[arity];
+                e.topSyms = new int[arity][];
+            }
+            for (int j = 0; j < arity; j++) {
+                v = reader.readInt();
+                e.nrTopSyms[j] = v;
+                e.symWidth[j] = bitWidth(v);
+                e.topSyms[j] = new int[v];
+
+                for (int k = 0; k < e.nrTopSyms[j]; k++) {
+                    v = reader.readInt();
+                    e.topSyms[j][k] = v;
+                }
+            }
+        }
+    }
+
+    private int bitWidth(int v) {
+        int nrBits = 0;
+
+        if (v <= 1)
+            return 0;
+
+        while (v != 0) {
+            v >>= 1;
+            nrBits++;
+        }
+
+        return nrBits;
+    }
+
+    private AFun readSymbol() throws IOException {
+        String s = reader.readString();
+        int arity = reader.readInt();
+        int quoted = reader.readInt();
+
+        debug(s + " / " + arity + " / " + quoted);
+
+        return factory.makeAFun(s, arity, quoted != 0);
+    }
+    
+    public static class BitStream {
+
+        InputStream stream;
+        private int bitsInBuffer;
+        private int bitBuffer;
+        
+        public BitStream(InputStream inputStream) {
+            stream = inputStream;
+        }
+
+        public int readInt() throws IOException {
+            int[] buf = new int[5];
+            
+            buf[0] = readByte();
+            
+            // Check if 1st character is enough
+            if((buf[0] & 0x80) == 0)
+                return buf[0];
+
+            buf[1]  = readByte();
+            
+            // Check if 2nd character is enough
+            if((buf[0] & 0x40) == 0)
+                return buf[1] + ((buf[0] & ~0xc0) << 8);
+
+            buf[2] = readByte();
+
+            // Check if 3rd character is enough
+            if((buf[0] & 0x20) == 0 )
+                return buf[2] + (buf[1] << 8) + ((buf[0] & ~0xe0) << 16);
+
+            buf[3] = readByte();
+            
+            // Check if 4th character is enough
+            if((buf[0] & 0x10) == 0 )
+                return buf[3] + (buf[2] << 8) + (buf[1] << 16) +
+                  ((buf[0] & ~0xf0) << 24);
+            
+            buf[4] = readByte();
+
+            return buf[4] + (buf[3] << 8) + (buf[2] << 16) + (buf[1] << 24);
+        }
+
+        private int readByte() throws IOException {
+            int c = stream.read();
+            if(c == -1)
+                throw new EOFException();
+            return c;
+        }
+
+        public String readString() throws IOException {
+            int l = readInt();
+            byte[] b = new byte[l];
+            stream.read(b, 0, b.length);
+            return new String(b);
+        }
+
+        public int readBits(int nrBits) throws IOException {
+            int mask = 1;
+            int val = 0;
+            
+            for (int i=0; i<nrBits; i++) {
+              if (bitsInBuffer == 0) {
+                int v = readByte();
+                if (v == -1)
+                  return -1;
+                bitBuffer = v;
+                bitsInBuffer = 8;
+              }
+              val |= (((bitBuffer & 0x80) != 0) ? mask : 0);
+              mask <<= 1;
+              bitBuffer <<= 1;
+              bitsInBuffer--;
+            }
+            
+            return val;
+        }
+
+        public void flushBitsFromReader() {
+            bitsInBuffer = 0;
+        }
+    }
+}
---
+++ org.spoofax.aterm/aterm/pure/TAFReader.java
@@ -0,0 +1,431 @@
+/*
+ * Created on 14.mar.2006
+ *
+ * Copyright (c) 2005, Karl Trygve Kalleberg <karltk@ii.uib.no>
+ * 
+ * Licensed under the GNU General Public License, v2
+ */
+package aterm.pure;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Vector;
+
+import aterm.ATerm;
+import aterm.ATermList;
+import aterm.ParseError;
+
+public class TAFReader {
+    
+    PureFactory factory;
+    // ATermReader reader;
+    
+    TAFReader(PureFactory factory) { //, ATermReader reader) {
+        this.factory = factory;
+        // this.reader = reader;
+    }
+    
+    static boolean isBase64(int c) {
+        return Character.isLetterOrDigit((char) c) || c == '+' || c == '/';
+    }
+    
+    public ATerm parse(String trm) {
+        try {
+            ATermReader reader = new ATermReader(new StringReader(trm));
+            reader.readSkippingWS();
+            ATerm result = parseFromReader(reader);
+            return result;
+        }
+        catch (IOException e) {
+            throw new ParseError("premature end of string");
+        }
+    }
+
+
+    private ATerm parseAbbrev(ATermReader reader) throws IOException {
+        ATerm result;
+        int abbrev;
+
+        int c = reader.read();
+
+        abbrev = 0;
+        while (isBase64(c)) {
+            abbrev *= 64;
+            if (c >= 'A' && c <= 'Z') {
+                abbrev += c - 'A';
+            }
+            else if (c >= 'a' && c <= 'z') {
+                abbrev += c - 'a' + 26;
+            }
+            else if (c >= '0' && c <= '9') {
+                abbrev += c - '0' + 52;
+            }
+            else if (c == '+') {
+                abbrev += 62;
+            }
+            else if (c == '/') {
+                abbrev += 63;
+            }
+            else {
+                throw new RuntimeException("not a base-64 digit: " + c);
+            }
+
+            c = reader.read();
+        }
+
+        result = reader.getTerm(abbrev);
+
+        return result;
+    }
+
+    private ATerm parseNumber(ATermReader reader) throws IOException {
+        StringBuffer str = new StringBuffer();
+        ATerm result;
+
+        do {
+            str.append((char) reader.getLastChar());
+        }
+        while (Character.isDigit((char) reader.read()));
+
+        if (reader.getLastChar() != '.' && reader.getLastChar() != 'e' && reader.getLastChar() != 'E') {
+            int val;
+            try {
+                val = Integer.parseInt(str.toString());
+            }
+            catch (NumberFormatException e) {
+                throw new ParseError("malformed int");
+            }
+            result = factory.makeInt(val);
+        }
+        else {
+            if (reader.getLastChar() == '.') {
+                str.append('.');
+                reader.read();
+                if (!Character.isDigit((char) reader.getLastChar()))
+                    throw new ParseError("digit expected");
+                do {
+                    str.append((char) reader.getLastChar());
+                }
+                while (Character.isDigit((char) reader.read()));
+            }
+            if (reader.getLastChar() == 'e' || reader.getLastChar() == 'E') {
+                str.append((char) reader.getLastChar());
+                reader.read();
+                if (reader.getLastChar() == '-' || reader.getLastChar() == '+') {
+                    str.append((char) reader.getLastChar());
+                    reader.read();
+                }
+                if (!Character.isDigit((char) reader.getLastChar()))
+                    throw new ParseError("digit expected!");
+                do {
+                    str.append((char) reader.getLastChar());
+                }
+                while (Character.isDigit((char) reader.read()));
+            }
+            double val;
+            try {
+                val = Double.valueOf(str.toString()).doubleValue();
+            }
+            catch (NumberFormatException e) {
+                throw new ParseError("malformed real");
+            }
+            result = factory.makeReal(val);
+        }
+        return result;
+    }
+
+    private String parseId(ATermReader reader) throws IOException {
+        int c = reader.getLastChar();
+        StringBuffer buf = new StringBuffer(32);
+
+        do {
+            buf.append((char) c);
+            c = reader.read();
+        }
+        while (Character.isLetterOrDigit((char) c) || c == '_' || c == '-');
+
+        return buf.toString();
+    }
+
+    private String parseString(ATermReader reader) throws IOException {
+        boolean escaped;
+        StringBuffer str = new StringBuffer();
+
+        do {
+            escaped = false;
+            if (reader.read() == '\\') {
+                reader.read();
+                escaped = true;
+            }
+
+            if (escaped) {
+                switch (reader.getLastChar()) {
+                    case 'n' :
+                        str.append('\n');
+                        break;
+                    case 't' :
+                        str.append('\t');
+                        break;
+                    case 'b' :
+                        str.append('\b');
+                        break;
+                    case 'r' :
+                        str.append('\r');
+                        break;
+                    case 'f' :
+                        str.append('\f');
+                        break;
+                    case '\\' :
+                        str.append('\\');
+                        break;
+                    case '\'' :
+                        str.append('\'');
+                        break;
+                    case '\"' :
+                        str.append('\"');
+                        break;
+                    case '0' :
+                    case '1' :
+                    case '2' :
+                    case '3' :
+                    case '4' :
+                    case '5' :
+                    case '6' :
+                    case '7' :
+                        str.append(reader.readOct());
+                        break;
+                    default :
+                        str.append('\\').append((char) reader.getLastChar());
+                }
+            }
+            else if (reader.getLastChar() != '\"')
+                str.append((char) reader.getLastChar());
+        }
+        while (escaped || reader.getLastChar() != '"');
+
+        return str.toString();
+    }
+
+    private ATermList parseATerms(ATermReader reader) throws IOException {
+        ATerm[] terms = parseATermsArray(reader);
+        ATermList result = factory.getEmpty();
+        for (int i = terms.length - 1; i >= 0; i--) {
+            result = factory.makeList(terms[i], result);
+        }
+
+        return result;
+    }
+
+    private ATerm[] parseATermsArray(ATermReader reader) throws IOException {
+        List list = new Vector();
+        ATerm term;
+
+        term = parseFromReader(reader);
+        list.add(term);
+        while (reader.getLastChar() == ',') {
+            reader.readSkippingWS();
+            term = parseFromReader(reader);
+            list.add(term);
+        }
+
+        ATerm[] array = new ATerm[list.size()];
+        ListIterator iter = list.listIterator();
+        int index = 0;
+        while (iter.hasNext()) {
+            array[index++] = (ATerm) iter.next();
+        }
+        return array;
+    }
+
+    synchronized ATerm parseFromReader(ATermReader reader) throws IOException {
+        ATerm result;
+        int c, start, end;
+        String funname;
+
+        start = reader.getPosition();
+        switch (reader.getLastChar()) {
+            case -1 :
+                throw new ParseError("premature EOF encountered.");
+
+            case '#' :
+                return parseAbbrev(reader);
+
+            case '[' :
+
+                c = reader.readSkippingWS();
+                if (c == -1) {
+                    throw new ParseError("premature EOF encountered.");
+                }
+
+                if (c == ']') {
+                    c = reader.readSkippingWS();
+                    result = (ATerm) factory.getEmpty();
+                }
+                else {
+                    result = parseATerms(reader);
+                    if (reader.getLastChar() != ']') {
+                        throw new ParseError("expected ']' but got '" + (char) reader.getLastChar() + "'");
+                    }
+                    c = reader.readSkippingWS();
+                }
+
+                break;
+
+            case '<' :
+
+                c = reader.readSkippingWS();
+                ATerm ph = parseFromReader(reader);
+
+                if (reader.getLastChar() != '>') {
+                    throw new ParseError("expected '>' but got '" + (char) reader.getLastChar() + "'");
+                }
+
+                c = reader.readSkippingWS();
+
+                result = factory.makePlaceholder(ph);
+
+                break;
+
+            case '"' :
+
+                funname = parseString(reader);
+
+                c = reader.readSkippingWS();
+                if (reader.getLastChar() == '(') {
+                    c = reader.readSkippingWS();
+                    if (c == -1) {
+                        throw new ParseError("premature EOF encountered.");
+                    }
+                    if (reader.getLastChar() == ')') {
+                        result = factory.makeAppl(factory.makeAFun(funname, 0, true));
+                    }
+                    else {
+                        ATerm[] list = parseATermsArray(reader);
+
+                        if (reader.getLastChar() != ')') {
+                            throw new ParseError("expected ')' but got '" + reader.getLastChar() + "'");
+                        }
+                        result = factory.makeAppl(factory.makeAFun(funname, list.length, true), list);
+                    }
+                    c = reader.readSkippingWS();
+                    if (c == -1) {
+                        throw new ParseError("premature EOF encountered.");
+                    }
+                }
+                else {
+                    result = factory.makeAppl(factory.makeAFun(funname, 0, true));
+                }
+
+                break;
+
+            case '(' :
+
+                c = reader.readSkippingWS();
+                if (c == -1) {
+                    throw new ParseError("premature EOF encountered.");
+                }
+                if (reader.getLastChar() == ')') {
+                    result = factory.makeAppl(factory.makeAFun("", 0, false));
+                }
+                else {
+                    ATerm[] list = parseATermsArray(reader);
+
+                    if (reader.getLastChar() != ')') {
+                        throw new ParseError("expected ')' but got '" + reader.getLastChar() + "'");
+                    }
+                    result = factory.makeAppl(factory.makeAFun("", list.length, false), list);
+                }
+                c = reader.readSkippingWS();
+                if (c == -1) {
+                    throw new ParseError("premature EOF encountered.");
+                }
+
+                break;
+
+            case '-' :
+            case '0' :
+            case '1' :
+            case '2' :
+            case '3' :
+            case '4' :
+            case '5' :
+            case '6' :
+            case '7' :
+            case '8' :
+            case '9' :
+                result = parseNumber(reader);
+                c = reader.skipWS();
+                break;
+
+            default :
+                c = reader.getLastChar();
+                if (Character.isLetter((char) c)) {
+
+                    funname = parseId(reader);
+                    c = reader.skipWS();
+                    if (reader.getLastChar() == '(') {
+                        c = reader.readSkippingWS();
+                        if (c == -1) {
+                            throw new ParseError("premature EOF encountered.");
+                        }
+                        if (reader.getLastChar() == ')') {
+                            result = factory.makeAppl(factory.makeAFun(funname, 0, false));
+                        }
+                        else {
+                            ATerm[] list = parseATermsArray(reader);
+
+                            if (reader.getLastChar() != ')') {
+                                throw new ParseError("expected ')' but got '" + reader.getLastChar() + "'");
+                            }
+                            result = factory.makeAppl(factory.makeAFun(funname, list.length, false), list);
+                        }
+                        c = reader.readSkippingWS();
+                    }
+                    else {
+                        result = factory.makeAppl(factory.makeAFun(funname, 0, false));
+                    }
+
+                }
+                else {
+                    throw new ParseError("illegal character: " + reader.getLastChar());
+                }
+        }
+
+        if (reader.getLastChar() == '{') {
+
+            ATermList annos;
+            if (reader.readSkippingWS() == '}') {
+                reader.readSkippingWS();
+                annos = factory.getEmpty();
+            }
+            else {
+                annos = parseATerms(reader);
+                if (reader.getLastChar() != '}') {
+                    throw new ParseError("'}' expected");
+                }
+                reader.readSkippingWS();
+            }
+            result = result.setAnnotations(annos);
+
+        }
+
+        /* Parse some ToolBus anomalies for backwards compatibility */
+        if (reader.getLastChar() == ':') {
+            reader.read();
+            ATerm anno = parseFromReader(reader);
+            result = result.setAnnotation(parse("type"), anno);
+        }
+
+        if (reader.getLastChar() == '?') {
+            reader.readSkippingWS();
+            result = result.setAnnotation(parse("result"), parse("true"));
+        }
+
+        end = reader.getPosition();
+        reader.storeNextTerm(result, end - start);
+
+        return result;
+    }
+}
