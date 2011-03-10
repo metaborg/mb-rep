3db47fddacf43524d634ee022c69b6f3202d6c71
Sverre Rabbelier <srabbelier@gmail.com>
2011-03-10 14:51:16 +0000

    Make org.spoofax.terms compilable under GWT
    svn path=/spoofax/trunk/spoofax/org.spoofax.terms/; revision=22350

---
+++ org.spoofax.terms/src/org/spoofax/gwt/org/spoofax/terms/attachments/TermAttachmentType.java
@@ -0,0 +1,46 @@
+package org.spoofax.terms.attachments;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Lennart Kats <lennart add lclnet.nl>
+ */
+public class TermAttachmentType<T extends ITermAttachment> {
+
+	private static Map<Class<?>, TermAttachmentType<?>> types =
+		new HashMap<Class<?>, TermAttachmentType<?>>();
+
+	private final Class<?> type;
+
+	private TermAttachmentType(Class<?> type) {
+		this.type = type;
+		// Private default constructor
+	}
+
+	/**
+	 * Creates a new attachment type. Can only create attachment types that are not
+	 * a superclass or subclass of another, existing attachment type.
+	 */
+	public synchronized static<T extends ITermAttachment> TermAttachmentType<T> create(Class<T> baseClass) {
+		@SuppressWarnings("unchecked")
+		TermAttachmentType<T> result = (TermAttachmentType<T>) types.get(baseClass);
+		if (result != null) {
+			return result;
+		} else {
+			result = new TermAttachmentType<T>(baseClass);
+			for (Class<?> otherClass : types.keySet()) {
+				if (otherClass.getName() == baseClass.getName())
+					throw new IllegalArgumentException("An attachment type based on class "
+							+ otherClass.getName() + " already exists; cannot add " + baseClass.getName());
+			}
+			types.put(baseClass, result);
+			return result;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return type.getName(); // (reflective call)
+	}
+}
