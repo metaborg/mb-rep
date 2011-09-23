00d25aca49af1d0efe289eedad84c8f33a640e9a
MdAdilAkhter <md.adilakhter@gmail.com>
2011-09-23 09:31:50 +0000

    primitive to get all the import references
    svn path=/spoofax/trunk/spoofax/org.spoofax.interpreter.library.language/; revision=23385

---
+++ org.spoofax.interpreter.library.language/src/org/spoofax/interpreter/library/language/spxlang/SPX_index_get_imports.java
@@ -0,0 +1,64 @@
+package org.spoofax.interpreter.library.language.spxlang;
+
+import org.spoofax.interpreter.core.IContext;
+import org.spoofax.interpreter.core.InterpreterException;
+import org.spoofax.interpreter.core.Tools;
+import org.spoofax.interpreter.library.AbstractPrimitive;
+import org.spoofax.interpreter.library.ssl.SSLLibrary;
+import org.spoofax.interpreter.stratego.Strategy;
+import org.spoofax.interpreter.terms.IStrategoAppl;
+import org.spoofax.interpreter.terms.IStrategoString;
+import org.spoofax.interpreter.terms.IStrategoTerm;
+
+/**
+ * Primitive to retrieve ModuleDeclaration from {@link SpxSemanticIndex}
+ * 
+ * @author Md. Adil Akhter
+ * Created On : Sep 11, 2011
+ */
+public class SPX_index_get_imports extends AbstractPrimitive {
+
+	private static String NAME = "SPX_index_get_imports";
+	private static int PROJECT_NAME_INDEX = 0;
+	private static int NAMESPACE_ID_INDEX = 1;
+	
+	private final static int NO_ARGS = 2;
+	
+	private final SpxSemanticIndex index;
+
+	public SPX_index_get_imports(SpxSemanticIndex index) {
+		super(NAME, 0, NO_ARGS);
+		this.index = index;
+	}
+	
+	/* Retrieve Spoofaxlang ModuleDeclaration with Module ID 
+	 * specified in {@code tvars}.    
+	 * 
+	 * {@code tvars} contains name of the project and typed qualified ModuleID  
+	 * @see org.spoofax.interpreter.library.AbstractPrimitive#call(org.spoofax.interpreter.core.IContext, org.spoofax.interpreter.stratego.Strategy[], org.spoofax.interpreter.terms.IStrategoTerm[])
+	 */
+	@Override
+	public boolean call(IContext env, Strategy[] svars, IStrategoTerm[] tvars){
+		boolean successStatement = false;
+		if ( (NO_ARGS == tvars.length) && Tools.isTermString(tvars[PROJECT_NAME_INDEX]) && Tools.isTermAppl(tvars[NAMESPACE_ID_INDEX])) 
+		{
+			IStrategoString projectName    = (IStrategoString)tvars[PROJECT_NAME_INDEX];
+			IStrategoAppl namespaceID = (IStrategoAppl)tvars[NAMESPACE_ID_INDEX];
+			try {
+				IStrategoTerm t = index.getImports(projectName, namespaceID);
+				env.setCurrent(t);
+				successStatement = true;
+			} 
+			catch(Exception ex)
+			{
+				// Logging any exception throw from the underlying symbol table. 
+				SSLLibrary.instance(env).getIOAgent().printError("["+NAME+"]  Invocation failed . "+ ex.getClass().getSimpleName() +" | error message: " + ex.getMessage());
+			}
+		}
+		else
+			SSLLibrary.instance(env).getIOAgent().printError("["+NAME+"]  Incokation failed . Error :  Mismatch in provided arguments. Variables provided : "+ tvars);
+		
+		return successStatement;
+	}
+
+}
