6189e5ae74b0d6d1a399e8a419986ce105eb8c8b
Gabriel Konat <gabrielkonat@gmail.com>
2013-07-11 15:38:04 -0700

    Add primitives for push, pop and merge.

--- org.spoofax.interpreter.library.index/src/main/java/org/spoofax/interpreter/library/index/primitives/IndexLibrary.java
+++ org.spoofax.interpreter.library.index/src/main/java/org/spoofax/interpreter/library/index/primitives/IndexLibrary.java
@@ -7,21 +7,31 @@ public class IndexLibrary extends AbstractStrategoOperatorRegistry {
 
     public IndexLibrary() {
         add(new LANG_index_add());
+        
         add(new LANG_index_clear_all());
         add(new LANG_index_clear_partition());
+        
         add(new LANG_index_get_all_partitions());
         add(new LANG_index_get_all_in_partition());
         add(new LANG_index_get_children());
         add(new LANG_index_get_partitions_of());
         add(new LANG_index_get());
+        
         add(new LANG_index_setup());
         add(new LANG_index_persist());
+        
         add(new LANG_index_get_current_partition());
         add(new LANG_index_set_current_partition());
+        
         add(new LANG_index_reload());
         add(new LANG_index_unload());
+        
         add(new LANG_index_start_collection());
         add(new LANG_index_stop_collection());
+        
+        add(new LANG_index_push());
+        add(new LANG_index_pop());
+        add(new LANG_index_merge());
     }
 
     public String getOperatorRegistryName() {
