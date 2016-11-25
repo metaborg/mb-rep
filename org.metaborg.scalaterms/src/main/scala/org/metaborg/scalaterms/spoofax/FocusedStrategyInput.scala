a56e32bd5c612de460305bcaef699b9eae282348
Jeff Smits <jeff.smits@gmail.com>
2016-11-25 16:37:46 +0100

    Added more Spoofax related STerms/Services + housekeeping

--- org.metaborg.scalaterms/src/main/scala/org/metaborg/scalaterms/spoofax/FocusedStrategyInput.scala
+++ org.metaborg.scalaterms/src/main/scala/org/metaborg/scalaterms/spoofax/FocusedStrategyInput.scala
@@ -1,6 +1,6 @@
 package org.metaborg.scalaterms.spoofax
 
-import org.metaborg.scalaterms.{ Extract, Origin, STerm }
+import org.metaborg.scalaterms.{FromSTerm, Origin, STerm, TermLikeCompanion}
 
 /**
   * Strategy input that's given, for example, to `editor-analyze`
@@ -21,10 +21,18 @@ case class FocusedStrategyInput(node: STerm,
   }
 }
 
-object ExtractFocusedStrategyInput extends Extract[FocusedStrategyInput] {
-  override def unapply(term: STerm): Option[FocusedStrategyInput] = term match {
-    case STerm.Tuple(List(node, position, ast, STerm.String(path, _), STerm.String(projectPath, _)), origin) => Some(
-      FocusedStrategyInput(node, position, ast, path, projectPath, origin))
-    case _                                                                                                   => None
+object FocusedStrategyInput extends TermLikeCompanion[FocusedStrategyInput] {
+  override val fromSTerm = new FromSTerm[FocusedStrategyInput] {
+    /**
+      * The extraction from STerm
+      *
+      * @param term the STerm
+      * @return the Some(T) that's extracted if matched
+      */
+    override def unapply(term: STerm): Option[FocusedStrategyInput] = term match {
+      case STerm.Tuple(List(node, position, ast, STerm.String(path, _), STerm.String(projectPath, _)), origin) => Some(
+        FocusedStrategyInput(node, position, ast, path, projectPath, origin))
+      case _                                                                                                   => None
+    }
   }
 }
---
+++ org.metaborg.scalaterms/src/main/scala/org/metaborg/scalaterms/spoofax/HoverResult.scala
@@ -0,0 +1,30 @@
+package org.metaborg.scalaterms.spoofax
+
+import org.metaborg.scalaterms._
+
+/**
+  * The Scala representation of the hover result message, as returned by `editor-hover`
+  */
+case class HoverResult(message: String) extends TermLike {
+  override val origin: Origin = null
+
+  /**
+    * @return equivalent Scala ATerm representation
+    */
+  override def toSTerm: STerm = STerm.String(message, origin)
+}
+
+object HoverResult extends TermLikeCompanion[HoverResult] {
+  override val fromSTerm = new FromSTerm[HoverResult] {
+    /**
+      * The extraction from STerm within a pattern match
+      *
+      * @param term the STerm
+      * @return the Some(T) that's extracted if matched
+      */
+    override def unapply(term: STerm): Option[HoverResult] = term match {
+      case STerm.String(message, _) => Some(HoverResult(message))
+      case _ => None
+    }
+  }
+}
