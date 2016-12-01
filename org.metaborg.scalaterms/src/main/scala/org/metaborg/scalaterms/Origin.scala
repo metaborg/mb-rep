89d4794438d611e4bfc8d40a9ebb0ec1f6834dbc
Jeff Smits <jeff.smits@gmail.com>
2016-12-01 15:01:00 +0100

    Preserve incoming null origins

--- org.metaborg.scalaterms/src/main/scala/org/metaborg/scalaterms/Origin.scala
+++ org.metaborg.scalaterms/src/main/scala/org/metaborg/scalaterms/Origin.scala
@@ -6,21 +6,31 @@ import org.spoofax.jsglr.client.imploder.ImploderAttachment
 /**
   * Scala representation of the Imploder Attachment on ATerms for the origin information (ImploderAttachment)
   */
-class Origin(filename: String, line: Int, column: Int, startOffset: Int, endOffset: Int) {
+class Origin(val filename: String, val line: Int, val column: Int, val startOffset: Int, val endOffset: Int) {
   def toStratego: ImploderAttachment = {
     ImploderAttachment.createCompactPositionAttachment(filename, line, column, startOffset, endOffset)
   }
-
-  def zero: Origin = new Origin(this.filename, 0, 0, 0, 0)
 }
 
 object Origin {
   def fromStratego(term: IStrategoTerm): Origin = {
     val origin = ImploderAttachment.getCompactPositionAttachment(term, false)
-    new Origin(filename = origin.getLeftToken.getTokenizer.getFilename,
-      line = origin.getLeftToken.getLine,
-      column = origin.getLeftToken.getColumn,
-      startOffset = origin.getLeftToken.getStartOffset,
-      endOffset = origin.getRightToken.getEndOffset)
+    if (origin == null) {
+      null
+    } else {
+      new Origin(filename = origin.getLeftToken.getTokenizer.getFilename,
+                 line = origin.getLeftToken.getLine,
+                 column = origin.getLeftToken.getColumn,
+                 startOffset = origin.getLeftToken.getStartOffset,
+                 endOffset = origin.getRightToken.getEndOffset)
+    }
+  }
+
+  def zero(o: Origin): Origin = {
+    if (o == null) {
+      null
+    } else {
+      new Origin(o.filename, 0, 0, 0, 0)
+    }
   }
 }
