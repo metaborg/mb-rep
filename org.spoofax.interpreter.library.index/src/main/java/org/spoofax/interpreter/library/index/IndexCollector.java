8f8de95d71e4b53cafb457f60d7089efe873b3da
Gabriel Konat <gabrielkonat@gmail.com>
2017-06-02 14:49:10 +0200

    Use reflection to support Guava 18 at runtime.

--- org.spoofax.interpreter.library.index/src/main/java/org/spoofax/interpreter/library/index/IndexCollector.java
+++ org.spoofax.interpreter.library.index/src/main/java/org/spoofax/interpreter/library/index/IndexCollector.java
@@ -1,5 +1,8 @@
 package org.spoofax.interpreter.library.index;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.IStrategoTuple;
 import org.spoofax.interpreter.terms.ITermFactory;
@@ -10,72 +13,93 @@ import com.google.common.collect.Multiset;
 import com.google.common.collect.Multisets;
 
 public class IndexCollector {
-	private final ITermFactory termFactory;
-	private final IndexEntryFactory entryFactory;
+    private final ITermFactory termFactory;
+    private final IndexEntryFactory entryFactory;
 
-	private final Multiset<IndexEntry> addedEntries = HashMultiset.create();
-	private final Multiset<IndexEntry> removedEntries = HashMultiset.create();
-	private final Multiset<IndexEntry> oldEntries = HashMultiset.create();
+    private final Multiset<IndexEntry> addedEntries = HashMultiset.create();
+    private final Multiset<IndexEntry> removedEntries = HashMultiset.create();
+    private final Multiset<IndexEntry> oldEntries = HashMultiset.create();
 
-	private IStrategoTerm sourceInCollection = null;
+    private IStrategoTerm sourceInCollection = null;
 
-	public IndexCollector(ITermFactory termFactory, IndexEntryFactory entryFactory) {
-		this.termFactory = termFactory;
-		this.entryFactory = entryFactory;
-	}
+    public IndexCollector(ITermFactory termFactory, IndexEntryFactory entryFactory) {
+        this.termFactory = termFactory;
+        this.entryFactory = entryFactory;
+    }
 
-	public void start(IStrategoTerm source, Iterable<IndexEntry> currentEntries) {
-		addedEntries.clear();
-		removedEntries.clear();
-		oldEntries.clear();
-		Iterables.addAll(removedEntries, currentEntries);
-		Iterables.addAll(oldEntries, currentEntries);
+    public void start(IStrategoTerm source, Iterable<IndexEntry> currentEntries) {
+        addedEntries.clear();
+        removedEntries.clear();
+        oldEntries.clear();
+        Iterables.addAll(removedEntries, currentEntries);
+        Iterables.addAll(oldEntries, currentEntries);
 
-		sourceInCollection = source;
-	}
+        sourceInCollection = source;
+    }
 
-	public IStrategoTuple stop() {
-		sourceInCollection = null;
+    public IStrategoTuple stop() {
+        sourceInCollection = null;
 
-		Multisets.removeOccurrences(addedEntries, oldEntries);
+        // Use reflection to choose right method to call, because in Guava 18 the type of the second argument of the
+        // removeOccurrences was changed.
+        final Method method = getRemoveOccurrencesMethod();
+        try {
+            method.invoke(null, addedEntries, oldEntries);
+        } catch(IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            throw new RuntimeException("Could not invoke remove occurrences", e);
+        }
 
-		// TODO: Use an IStrategoList implementation that iterates over the collections instead of constructing it.
-		return termFactory.makeTuple(entryFactory.toKeyTerms(removedEntries), entryFactory.toKeyTerms(addedEntries));
-	}
+        // TODO: Use an IStrategoList implementation that iterates over the collections instead of constructing it.
+        return termFactory.makeTuple(entryFactory.toKeyTerms(removedEntries), entryFactory.toKeyTerms(addedEntries));
+    }
 
-	public IndexEntry add(IStrategoTerm key, IStrategoTerm value) {
-		return add(entryFactory.create(key, value, sourceInCollection));
-	}
+    public IndexEntry add(IStrategoTerm key, IStrategoTerm value) {
+        return add(entryFactory.create(key, value, sourceInCollection));
+    }
 
-	public IndexEntry add(IStrategoTerm key) {
-		return add(entryFactory.create(key, sourceInCollection));
-	}
+    public IndexEntry add(IStrategoTerm key) {
+        return add(entryFactory.create(key, sourceInCollection));
+    }
 
-	public IndexEntry add(IndexEntry entry) {
-		assert entry.source.match(sourceInCollection);
+    public IndexEntry add(IndexEntry entry) {
+        assert entry.source.match(sourceInCollection);
 
-		addedEntries.add(entry);
-		removedEntries.remove(entry);
-		return entry;
-	}
+        addedEntries.add(entry);
+        removedEntries.remove(entry);
+        return entry;
+    }
 
-	public Iterable<IndexEntry> getAddedEntries() {
-		return addedEntries;
-	}
+    public Iterable<IndexEntry> getAddedEntries() {
+        return addedEntries;
+    }
 
-	public boolean inCollection() {
-		return sourceInCollection != null;
-	}
+    public boolean inCollection() {
+        return sourceInCollection != null;
+    }
 
-	public void recover() {
-	    reset();
-	}
-	
-	public void reset() {
-		addedEntries.clear();
-		removedEntries.clear();
-		oldEntries.clear();
+    public void recover() {
+        reset();
+    }
 
-		sourceInCollection = null;
-	}
+    public void reset() {
+        addedEntries.clear();
+        removedEntries.clear();
+        oldEntries.clear();
+
+        sourceInCollection = null;
+    }
+
+
+    private static Method getRemoveOccurrencesMethod() {
+        final String methodName = "removeOccurrences";
+        try {
+            return Multisets.class.getDeclaredMethod(methodName, Multiset.class, Multiset.class);
+        } catch(NoSuchMethodException e1) {
+            try {
+                return Multisets.class.getDeclaredMethod(methodName, Multiset.class, Iterable.class);
+            } catch(NoSuchMethodException e2) {
+                throw new RuntimeException("Cannot find " + methodName + " method via reflection", e2);
+            }
+        }
+    }
 }
