22f5d6510169c1be92503d916236fe2c02020942
Gabriel Konat <gabrielkonat@gmail.com>
2017-10-05 14:17:43 +0200

    Fix missing hashCode and equals implementations.

--- org.spoofax.terms/src/org/spoofax/terms/attachments/ParentAttachment.java
+++ org.spoofax.terms/src/org/spoofax/terms/attachments/ParentAttachment.java
@@ -138,7 +138,37 @@ public class ParentAttachment extends AbstractTermAttachment {
 		return null;
 	}
 	
-	@Override
+	
+	@Override public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((elementParent == null) ? 0 : elementParent.hashCode());
+        result = prime * result + ((parent == null) ? 0 : parent.hashCode());
+        return result;
+    }
+
+    @Override public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        if(obj == null)
+            return false;
+        if(getClass() != obj.getClass())
+            return false;
+        ParentAttachment other = (ParentAttachment) obj;
+        if(elementParent == null) {
+            if(other.elementParent != null)
+                return false;
+        } else if(!elementParent.equals(other.elementParent))
+            return false;
+        if(parent == null) {
+            if(other.parent != null)
+                return false;
+        } else if(!parent.equals(other.parent))
+            return false;
+        return true;
+    }
+
+    @Override
 	public String toString() {
 		return "parent: " + getParent();
 	}
