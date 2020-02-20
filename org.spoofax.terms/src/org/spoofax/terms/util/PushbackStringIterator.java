d86449aaa73ad960249c215a9f9dc2d2dcf4bec1
Maarten Sijm <mpsijm@gmail.com>
2020-02-20 13:04:40 +0100

    Remove no-longer-needed PushbackStringIterator.truncateUnicodeChar

--- org.spoofax.terms/src/org/spoofax/terms/util/PushbackStringIterator.java
+++ org.spoofax.terms/src/org/spoofax/terms/util/PushbackStringIterator.java
@@ -2,12 +2,6 @@ package org.spoofax.terms.util;
 
 public class PushbackStringIterator {
 
-	public static final int UNICODE_LETTER = 255;
-
-	public static final int UNICODE_DIGIT = 254;
-
-	public static final int UNICODE_OTHER = 253;
-
 	private final String data;
 	private int position;
 
@@ -23,20 +17,6 @@ public class PushbackStringIterator {
 		int c = data.codePointAt(position);
 		position += Character.charCount(c);
 		return c;
-	}
-
-	// TODO should not be needed anymore, but is used in JSGLR1 because it does not support Unicode
-	public char truncateUnicodeChar(int c) {
-		if (c >= UNICODE_OTHER) {
-			if (Character.isLetter(c)) {
-				return UNICODE_LETTER;
-			} else if (Character.isDigit(c)) {
-				return UNICODE_DIGIT;
-			} else {
-				return UNICODE_OTHER;
-			}
-		}
-		return (char) c;
 	}
 
 	public void unread(int c) {
