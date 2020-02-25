c6028f038489bca5e2a8476d22d4b0bd3310bf42
Virtlink <647530+Virtlink@users.noreply.github.com>
2020-02-25 17:13:26 +0100

    Extend NotImplementedException from UnsupportedOperationException

--- org.spoofax.terms/src/org/spoofax/terms/util/NotImplementedException.java
+++ org.spoofax.terms/src/org/spoofax/terms/util/NotImplementedException.java
@@ -7,15 +7,31 @@
  */
 package org.spoofax.terms.util;
 
-public class NotImplementedException extends RuntimeException {
-    
-    public NotImplementedException() {
-        super("Not Implemented");
-    }
-    
-    public NotImplementedException(String message) {
-        super(message);
-    }
+import javax.annotation.Nullable;
+
+/**
+ * Exception indicating that something was not implemented.
+ */
+public class NotImplementedException extends UnsupportedOperationException {
 
     private static final long serialVersionUID = -1028814795329444374L;
+    private static final String DEFAULT_MESSAGE = "This operation has not been implemented.";
+
+    /**
+     * Initializes a new instance of the {@link NotImplementedException} class.
+     *
+     * @param message the message to display; or {@code null} to use the default message
+     */
+    public NotImplementedException(@Nullable String message) {
+        super(message != null ? message : DEFAULT_MESSAGE, null);
+    }
+
+    /**
+     * Initializes a new instance of the {@link NotImplementedException} class
+     * with the default message.
+     */
+    public NotImplementedException() {
+        this(null);
+    }
+
 }
