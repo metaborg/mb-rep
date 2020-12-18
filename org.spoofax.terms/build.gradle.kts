ab8aa29bc4de7d2a6c8905539c3a9f16989dde30
Gabriel Konat <gabrielkonat@gmail.com>
2020-12-18 15:55:36 +0100

    Add Gradle build files.

---
+++ org.spoofax.terms/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+  id("org.metaborg.gradle.config.java-library")
+  id("org.metaborg.gradle.config.junit-testing")
+}
+
+sourceSets {
+  main {
+    java {
+      srcDir("src")
+    }
+  }
+  test {
+    java {
+      srcDir("test")
+    }
+  }
+}
+
+val spoofax2Version: String by ext
+dependencies {
+  api(platform("org.metaborg:parent:$spoofax2Version"))
+
+  compileOnly("com.google.code.findbugs:jsr305")
+
+  testCompileOnly("com.google.code.findbugs:jsr305")
+}
+
+// Copy test resources into classes directory, to make them accessible as classloader resources at runtime.
+val copyTestResourcesTask = tasks.create<Copy>("copyTestResources") {
+  from("$projectDir/test-resources")
+  into("$buildDir/classes/java/test")
+}
+tasks.getByName("processTestResources").dependsOn(copyTestResourcesTask)
