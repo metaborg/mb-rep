0d824e7ae71f176ed306c218917bf61fd0dccb90
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2021-03-15 14:26:16 +0100

    Placeholders can be list tails
    Users that don't know about list placeholders will see an empty list.

--- org.spoofax.terms/src/org/spoofax/interpreter/terms/IStrategoPlaceholder.java
+++ org.spoofax.terms/src/org/spoofax/interpreter/terms/IStrategoPlaceholder.java
@@ -1,5 +1,9 @@
 package org.spoofax.interpreter.terms;
 
+import org.spoofax.terms.StrategoList;
+
+import java.util.NoSuchElementException;
+
 /**
  * A Stratego Placeholder term.
  *
@@ -8,7 +12,7 @@ package org.spoofax.interpreter.terms;
  * They are most useful in the C implementation of ATerms, and are not really used in the Java
  * implementation.
  */
-public interface IStrategoPlaceholder extends IStrategoTerm {
+public interface IStrategoPlaceholder extends IStrategoTerm, IStrategoList {
 
     /**
      * Gets the template of the placeholder.
@@ -17,4 +21,26 @@ public interface IStrategoPlaceholder extends IStrategoTerm {
      */
     IStrategoTerm getTemplate();
 
+    @Override
+    default int size() { return 0; }
+
+    @Override
+    default IStrategoTerm head() { throw new NoSuchElementException(); }
+
+    @Override
+    default IStrategoList tail() { throw new IllegalStateException(); }
+
+    @Override
+    default boolean isEmpty() { return true; }
+
+    @Override
+    default IStrategoTerm get(int index) {
+        throw new IndexOutOfBoundsException("List placeholder has no terms.");
+    }
+
+    @Deprecated
+    @Override
+    default IStrategoList prepend(IStrategoTerm prefix) {
+        return new StrategoList(prefix, this, null);
+    }
 }
