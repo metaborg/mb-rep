fb695ce0b16ac0c9b11fc8b9363bcbdc7d62ccd1
Jeff Smits <mail@jeffsmits.net>
2023-04-17 17:45:38 +0200

    Use new mutable setmultimap

--- org.spoofax.interpreter.library.index/src/main/java/org/spoofax/interpreter/library/index/Index.java
+++ org.spoofax.interpreter.library.index/src/main/java/org/spoofax/interpreter/library/index/Index.java
@@ -4,16 +4,16 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.metaborg.util.collection.MultiSet;
+import org.metaborg.util.collection.SetMultimap;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.IStrategoTuple;
 import org.spoofax.interpreter.terms.ITermFactory;
 
-import io.usethesource.capsule.SetMultimap;
-
 public class Index implements IIndex {
-    private SetMultimap.Transient<IStrategoTerm, IndexEntry> entries = SetMultimap.Transient.of();
-    private SetMultimap.Transient<IStrategoTerm, IndexEntry> childs = SetMultimap.Transient.of();
-    private SetMultimap.Transient<IStrategoTerm, IndexEntry> entriesPerSource = SetMultimap.Transient.of();
+    private final SetMultimap<IStrategoTerm, IndexEntry> entries = new SetMultimap<>();
+    private final SetMultimap<IStrategoTerm, IndexEntry> childs = new SetMultimap<>();
+    private final SetMultimap<IStrategoTerm, IndexEntry> entriesPerSource = new SetMultimap<>();
 
     private final IndexEntryFactory entryFactory;
     private final IndexParentKeyFactory parentKeyFactory;
@@ -50,27 +50,26 @@ public class Index implements IIndex {
     }
 
     @Override public void add(IndexEntry entry) {
-        entries.__insert(entry.key, entry);
+        entries.put(entry.key, entry);
 
         final IStrategoTerm parentKey = parentKeyFactory.getParentKey(entry.key);
         if(parentKey != null) {
-            childs.__insert(parentKey, entry);
+            childs.put(parentKey, entry);
         }
 
-        entriesPerSource.__insert(entry.source, entry);
+        entriesPerSource.put(entry.source, entry);
     }
 
     @Override public void addAll(IStrategoTerm source, Iterable<IndexEntry> entriesToAdd) {
-        final Collection<IndexEntry> entriesInSource = entriesPerSource.get(source);
         for(final IndexEntry entry : entriesToAdd) {
-            entries.__insert(entry.key, entry);
+            entries.put(entry.key, entry);
 
             final IStrategoTerm parentKey = parentKeyFactory.getParentKey(entry.key);
             if(parentKey != null) {
-                childs.__insert(parentKey, entry);
+                childs.put(parentKey, entry);
             }
 
-            entriesInSource.add(entry);
+            entriesPerSource.put(source, entry);
         }
     }
 
@@ -100,13 +99,13 @@ public class Index implements IIndex {
 
     @Override public void clearSource(IStrategoTerm source) {
         for(final IndexEntry entry : getInSource(source)) {
-            entries.__remove(entry.key, entry);
+            entries.remove(entry.key, entry);
             final IStrategoTerm parentKey = parentKeyFactory.getParentKey(entry.key);
             if(parentKey != null) {
-                childs.__remove(parentKey, entry);
+                childs.remove(parentKey, entry);
             }
         }
-        entriesPerSource.__remove(source);
+        entriesPerSource.remove(source);
     }
 
     @Override public Iterable<IStrategoTerm> getAllSources() {
@@ -118,9 +117,9 @@ public class Index implements IIndex {
     }
 
     @Override public void reset() {
-        entries = SetMultimap.Transient.of();
-        childs = SetMultimap.Transient.of();
-        entriesPerSource = SetMultimap.Transient.of();
+        entries.clear();
+        childs.clear();
+        entriesPerSource.clear();
         collector.reset();
     }
 }
