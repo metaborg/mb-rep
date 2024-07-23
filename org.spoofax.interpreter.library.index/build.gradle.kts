4c4104e54f680f245b49e846efdc08c91a640fb9
Jeff Smits <j.smits-1@tudelft.nl>
2024-07-23 13:19:18 +0200

    Merge branch 'master' into spoofax-release

--- org.spoofax.interpreter.library.index/build.gradle.kts
+++ org.spoofax.interpreter.library.index/build.gradle.kts
@@ -1,26 +1,26 @@
 plugins {
-  id("org.metaborg.gradle.config.java-library")
-  id("org.metaborg.gradle.config.junit-testing")
+    id("org.metaborg.gradle.config.java-library")
+    id("org.metaborg.gradle.config.junit-testing")
 }
 
 fun compositeBuild(name: String) = "$group:$name:$version"
 val spoofax2Version: String by ext
 dependencies {
-  api(platform("org.metaborg:parent:$spoofax2Version"))
+    api(platform("org.metaborg:parent:$spoofax2Version"))
 
-  api(project(":org.spoofax.terms"))
-  implementation(compositeBuild("jsglr.shared"))
-  api(compositeBuild("org.spoofax.interpreter.core"))
-  implementation(compositeBuild("org.metaborg.util"))
+    api(project(":org.spoofax.terms"))
+    implementation(compositeBuild("jsglr.shared"))
+    api(compositeBuild("org.spoofax.interpreter.core"))
+    implementation(compositeBuild("org.metaborg.util"))
 
-  implementation("jakarta.annotation:jakarta.annotation-api")
+    implementation("jakarta.annotation:jakarta.annotation-api")
 
-  testCompileOnly("junit:junit")
-  testImplementation("com.carrotsearch:junit-benchmarks")
-  testCompileOnly("jakarta.annotation:jakarta.annotation-api")
-  testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.1.0")
+    testCompileOnly("junit:junit")
+    testImplementation("com.carrotsearch:junit-benchmarks")
+    testCompileOnly("jakarta.annotation:jakarta.annotation-api")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.1.0")
 }
 
 tasks.test {
-  exclude("**/performance/**") // Ignore performance benchmarks.
+    exclude("**/performance/**") // Ignore performance benchmarks.
 }
--- org.spoofax.terms/build.gradle.kts
+++ org.spoofax.terms/build.gradle.kts
@@ -1,33 +1,33 @@
 plugins {
-  id("org.metaborg.gradle.config.java-library")
-  id("org.metaborg.gradle.config.junit-testing")
+    id("org.metaborg.gradle.config.java-library")
+    id("org.metaborg.gradle.config.junit-testing")
 }
 
 sourceSets {
-  main {
-    java {
-      srcDir("src")
+    main {
+        java {
+            srcDir("src")
+        }
     }
-  }
-  test {
-    java {
-      srcDir("test")
+    test {
+        java {
+            srcDir("test")
+        }
     }
-  }
 }
 
 val spoofax2Version: String by ext
 dependencies {
-  api(platform("org.metaborg:parent:$spoofax2Version"))
+    api(platform("org.metaborg:parent:$spoofax2Version"))
 
-  implementation("jakarta.annotation:jakarta.annotation-api")
+    implementation("jakarta.annotation:jakarta.annotation-api")
 
-  testCompileOnly("jakarta.annotation:jakarta.annotation-api")
+    testCompileOnly("jakarta.annotation:jakarta.annotation-api")
 }
 
 // Copy test resources into classes directory, to make them accessible as classloader resources at runtime.
 val copyTestResourcesTask = tasks.create<Copy>("copyTestResources") {
-  from("$projectDir/test-resources")
-  into("$buildDir/classes/java/test")
+    from("$projectDir/test-resources")
+    into("$buildDir/classes/java/test")
 }
 tasks.getByName("processTestResources").dependsOn(copyTestResourcesTask)
